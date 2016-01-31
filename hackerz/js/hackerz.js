4df09ca2a14b400eff90cb61a374a1c5b0c6f549
Michael <themichaeleden@gmail.com>
2016-01-31 13:56:47 -0500

    rewrite for bug fixes, better practice, and style

--- hackerz/js/hackerz.js
+++ hackerz/js/hackerz.js
@@ -1,5 +1,12 @@
 (function() {
     "use strict";
+    var DEBUG = false;
+
+    var Log = {};
+    var prompt_queue = {
+        password: [],
+    };
+    var menu = {};
 
     // Make a mock if we're in a browser
     if (!window.lightdm) {
@@ -33,7 +40,7 @@
                 }, 0);
             },
             respond: function(text) {
-                console.log("response", text);
+                Log.log("response", text);
                 if (auth_start) {
                     if (text === "password") {
                         window.lightdm.is_authenticated = true;
@@ -49,261 +56,205 @@
             },
         };
     }
-})();
 
-// Initial Metadata for the menu
-// var local = localStorage.getItem("menu");
-var DEBUG = false;
-var local = { up: undefined };
-var menu  = local.up != undefined ? local : {
-    up: false,
-    user: {
-        class: "user",
-        curr: 1,
-        max: 1
-    },
-    session: {
-        class: "session",
-        curr: 1,
-        max: 1
-    }
-};
-menu.up = false;
-var curr = menu.session;
-var currUser;
-var message_queue = {
-    password: [],
-};
+    // Generate menu
+    (function() {
+        menu.sessions = {};
+        for (var s = 0; s < lightdm.sessions.length; ++s) {
+            menu.sessions[s] = lightdm.sessions[s].name;
+        }
 
-function typeWriter(element, text, n, callback) {
-    if (n < text.length) {
-        element.innerHTML = text.substring(0, n + 1);
-        n++;
-        setTimeout(function() { typeWriter(element, text, n, callback); }, 50);
-    } else {
-        callback();
-    }
-}
+        menu.users = {};
+        for (var u = 0; u < lightdm.users.length; ++u) {
+            menu.users[u] = lightdm.users[u].name;
+        }
+        // Default user and session
+        menu.users.curr = 0;
+        menu.sessions.curr = 0;
+        menu.users.length = lightdm.users.length;
+        menu.sessions.length = lightdm.sessions.length;
+        menu.users.element = ".chooser ul.user li";
+        menu.sessions.element = ".chooser ul.session li";
+        // Most likely going to be changing the session
+        menu.column = {
+            0: "users",
+            1: "sessions",
+            curr: 1,
+            length: 2,
+        };
 
-function getDefaultUsername() {
-    return lightdm.users[menu.user.curr - 1].name;
-}
-
-function usernames() {
-    users = [];
-    for (i in lightdm.users) {
-        users.push(lightdm.users[i].name);
-    }
-    return users;
-}
-
-function login() {
-    // get references to objects;
-    var uInput = document.getElementById("user");
-    var pInput = document.getElementById("pass");
-
-    // Capture credentials
-    var user = uInput.value;
-    var pass = pInput.value;
-
-    console.log("Logging in with user", user);
-
-    // login n" such
-    lightdm.start_authentication(user);
-
-    console.log("Started authentication", user, "...");
-
-    // Some notifications:
-    $("#userLbl").html("Signing in...");
-    $("#passLbl").html("&nbsp;");
-
-    $("#user").attr("disabled", "disabled");
-    $("#pass").attr("disabled", "disabled");
-
-    message_queue.password.push(function() {
-        console.log("Sending credentials...");
-        return pass;
-    });
-}
-
-(function() {
-    // Don't set this up if we're not in debug mode.
-    if (!DEBUG) {
-        return;
-    }
-    $(document).ready(function() {
-        $(".debug").show();
-    });
-    var cache;
-    window.console.log = function() {
-        var args = Array.prototype.slice.call(arguments, 0);
-        var message = args.join(" ");
-        var logger = document.getElementById("logger");
-        if (logger) {
-            if (cache) {
-                message = cache + "<br/>" + message;
-                cache = undefined;
+        menu.update = function() {
+            Array.prototype.forEach.call(menu.column, function(column) {
+                menu[column].curr = menu[column].curr % menu[column].length;
+                if (menu[column].curr < 0) {
+                    menu[column].curr += menu[column].length;
+                }
+                $(menu[column].element).removeClass("selected");
+                $(menu[column].element).eq(menu[column].curr).addClass("selected");
+            });
+            if (menu.users.last !== menu.users.curr) {
+                $("#user").val(menu.users[menu.users.curr]);
             }
-            logger.innerHTML += message + "<br/>";
-        } else {
-            cache += "<br/>" + message;
-        }
-    };
-})();
+            menu.users.last = menu.users.curr;
+        };
+    })();
 
-window.onerror = function(message, url, line, column, error) {
-    // console.log("UNCAUGHT ERROR", JSON.stringify(arguments));
-    console.log("UNCAUGHT ERROR", line, ":", column, "@");
-};
-
-window.onload = function() {
-    console.log("Debugger Ready...");
-    for (key in lightdm) {
-        console.log(key);
-    }
-};
-
-function authentication_complete() {
-    var reenable_inputs = function() {
-        $("#user").removeAttr("disabled");
-        $("#pass").removeAttr("disabled");
-    };
-    if (lightdm.is_authenticated) {
-        // Pick which environment we want
-        $(".container").addClass("animated fadeOut");
-        setTimeout(function() {
-            // Login the user
-            // TODO: Should there be a delay for the fade effect?
-            var session = lightdm.sessions[menu.session.curr - 1].key;
-            lightdm.login(lightdm.authentication_user, session);
-        }, 500);
-    } else {
-        // Make an error-ing animation
-        // TODO: Make the password field highlighted when this happens
-        $(".container").attr("class", "container");
-        $("#userLbl").html("Username:");
-        $("#passLbl").html("Password:");
-        $("#pass").attr("class", "mono animated wobble");
-        setTimeout(function() {
-            $("#pass").attr("class", "mono");
-            $("#pass").val("");
-            reenable_inputs();
-
+    var type_writer = function(element, text, n, callback) {
+        if (n < text.length) {
+            element.html(text.substring(0, n + 1));
+            n += 1;
             setTimeout(function() {
+                type_writer(element, text, n, callback);
+            }, 50);
+        } else {
+            callback();
+        }
+    };
+
+    var login = function() {
+        // get references to objects;
+        var uInput = document.getElementById("user");
+        var pInput = document.getElementById("pass");
+
+        // Capture credentials
+        var user = uInput.value;
+        var pass = pInput.value;
+
+        Log.log("Logging in with user", user);
+
+        // login n" such
+        lightdm.start_authentication(user);
+
+        Log.log("Started authentication", user, "...");
+
+        // Some notifications:
+        $("#userLbl").html("Signing in...");
+        $("#passLbl").html("&nbsp;");
+
+        $("#user").attr("disabled", "disabled");
+        $("#pass").attr("disabled", "disabled");
+
+        prompt_queue.password.push(function() {
+            Log.log("Sending credentials...");
+            return pass ? pass : true;
+        });
+    };
+
+    window.show_prompt = function(text, type) {
+        Log.log("PROMPT " + type + ":", text);
+        var listeners = prompt_queue[type];
+        while (listeners.length > 0) {
+            var response = (listeners.pop())(text);
+            if (response) {
+                lightdm.respond(response);
+                break;
+            }
+        }
+    };
+
+    window.show_error = function(text, type) {
+        Log.log("ERROR" + type + ":", text);
+    };
+
+    window.onerror = function(message, url, line, column, error) {
+        Log.log("UNCAUGHT ERROR", error.stack);
+    };
+
+    window.authentication_complete = function() {
+        if (lightdm.is_authenticated) {
+            // Pick which environment we want
+            $(".container").addClass("animated fadeOut");
+            setTimeout(function() {
+                // Login the user
+                var session = menu.sessions[menu.sessions.curr];
+                lightdm.login(lightdm.authentication_user, session);
+            }, 500);
+        } else {
+            // Make an error-ing animation
+            $(".container").attr("class", "container");
+            $("#userLbl").html("Username:");
+            $("#passLbl").html("Password:");
+            $("#pass").attr("class", "mono animated wobble");
+            $("#user").removeAttr("disabled");
+            $("#pass").removeAttr("disabled");
+            $("#pass").val("");
+            $("#pass").focus();
+            setTimeout(function() {
+                $("#pass").attr("class", "mono");
+            }, 1000);
+        }
+    };
+
+    $(document).ready(function() {
+        Log.log("Debugger Ready...");
+        Array.prototype.forEach.call(menu.sessions, function(session) {
+            $(".chooser ul.session").append("<li>" + session + "</li>");
+        });
+        Array.prototype.forEach.call(menu.users, function(user) {
+            $(".chooser ul.user").append("<li>" + user + "</li>");
+        });
+        menu.update();
+
+        type_writer($("#userLbl"), "Username:", 0, function() {
+            type_writer($("#passLbl"), "Password:", 0, function() {
+                $("#user").addClass("mono animated fadeInUp");
+                $("#pass").addClass("mono animated fadeInDown");
+                $("#user, #pass").removeClass("hide");
                 $("#pass").focus();
-            }, 0);
-        }, 1000);
-        updateUser(currUser);
-    }
-}
+            });
+        });
 
-function updateUser(username) {
-    $("#user").val(username);
-    currUser = username;
-}
+        $(document).keydown(function(event) {
+            if (event.which === 13) {
+                login();
+            } else if (!event.ctrlKey) {
+                // We must be holding it down to edit the settings
+                return;
+            }
+            if (event.which == 17) {
+                $(".chooser").attr("class", "chooser animated flipInX");
+            } else if (event.which === 37 || event.which === 72) { // Left
+                menu.column.curr -= 1;
+            } else if (event.which === 39 || event.which === 76) { // Right
+                menu.column.curr += 1;
+            } else if (event.which === 38 || event.which === 75) { // Up
+                menu[menu.column[menu.column.curr]].curr -= 1;
+            } else if (event.which === 40 || event.which === 74) { // Down
+                menu[menu.column[menu.column.curr]].curr += 1;
+            }
+            menu.update();
+        });
 
-$(document).ready(function() {
-    for (i in lightdm.sessions) {
-        var active = "";
-        if (i == menu.session.curr - 1) {
-            active = "selected";
-        }
-        $(".chooser ul.session").append("<li class='" + active + "'>" + lightdm.sessions[i].name + "</li>");
-    }
-    menu.session.max = lightdm.sessions.length;
-
-    for (i in lightdm.users) {
-        var active = "";
-        if (i == menu.user.curr - 1) {
-            active = "selected";
-        }
-        $(".chooser ul.user").append("<li class='" + active + "'>" + lightdm.users[i].name + "</li>");
-    }
-    menu.user.max = lightdm.users.length;
-
-    typeWriter(document.getElementById("userLbl"), "Username:", 0, function() {
-        typeWriter(document.getElementById("passLbl"), "Password:", 0, function() {
-
-            currUser = getDefaultUsername();
-            document.getElementById("user").value = currUser;
-
-            inputs = document.getElementsByTagName("input");
-            inputs[0].className = "mono animated fadeInUp";
-            inputs[1].className = "mono animated fadeInDown";
-            inputs[1].focus();
+        $(document).keyup(function(event) {
+            if (event.which == 17) {
+                $(".chooser").attr("class", "chooser animated fadeOut");
+            }
         });
     });
 
-    function changeMenu(increment) {
-        var selector = ".chooser ul." + curr.class + " li:nth-child(";
-
-        $(selector + curr.curr + ")").removeClass("selected");
-        curr.curr += increment;
-        $(selector + curr.curr + ")").addClass("selected");
-
-        // Save to local storage
-        localStorage.setItem("menu", menu);
-        // Change user name if applicable
-        if (curr === menu.user) {
-            var user = $(selector + curr.curr + ")").html();
-            updateUser(user);
+    (function() {
+        // Don't set this up if we're not in debug mode.
+        if (DEBUG) {
+            $(document).ready(function() {
+                $(".debug").show();
+            });
+            var cache;
+            Log.log = function() {
+                var args = Array.prototype.slice.call(arguments, 0);
+                var message = args.join(" ");
+                var logger = document.getElementById("logger");
+                if (logger) {
+                    if (cache) {
+                        message = cache + "<br/>" + message;
+                        cache = undefined;
+                    }
+                    logger.innerHTML += message + "<br/>";
+                } else {
+                    cache += "<br/>" + message;
+                }
+            };
+        } else {
+            Log.log = function() {};
         }
-    }
-
-    $(document).keydown(function(event) {
-        if (event.which == 17) {
-            $(".chooser").attr("class", "chooser animated flipInX");
-            menu.up = true;
-        } else if (event.which === 37 || event.which === 72) {
-            // Left
-            if (menu.up && curr === menu.session) {
-                curr = menu.user;
-            }
-        } else if (event.which === 39 || event.which === 76) {
-            // Right
-            if (menu.up && curr === menu.user) {
-                curr = menu.session;
-            }
-        } else if (event.which === 38 || event.which === 75) {
-            // Up
-            if (!menu.up || curr.curr <= 1) return;
-            changeMenu(-1);
-        } else if (event.which === 40 || event.which === 74) {
-            // Down
-            if (!menu.up || curr.curr >= curr.max) return;
-            changeMenu(1);
-        }
-    });
-
-    $(document).keyup(function(event) {
-        if (event.which == 17) {
-            $(".chooser").attr("class", "chooser animated fadeOut");
-            menu.up = false;
-        }
-    });
-
-    var login_on_enter = function(event) {
-        if (event.which == 13) {
-            login();
-        }
-    };
-
-    // Enter accepts the password
-    $("#pass").keypress(login_on_enter);
-    $("#user").keypress(login_on_enter);
-});
-
-function show_prompt(text, type) {
-    console.log("PROMPT " + type + ":", text);
-    var listeners = message_queue[type];
-    while (listeners.length > 0) {
-        var response = (listeners.pop())(text);
-        if (response) {
-            lightdm.respond(response);
-            break;
-        }
-    }
-}
-function show_error(text, type) {
-    console.log("ERROR" + type + ":", text);
-}
+    })();
+})();
